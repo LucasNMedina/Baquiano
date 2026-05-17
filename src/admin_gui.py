3b475badc9dbc5ef86b731350ea6a43e020dbb1e
Lucas <lucasnahuelmedina@hotmail.com>
2026-05-16 22:49:19 -0300

    continue el admin_gui, casi terminando el boton agregar

--- src/admin_gui.py
+++ src/admin_gui.py
@@ -7,11 +7,65 @@ class AgregarAPP(ctk.CTkToplevel):
     def __init__(self, parent, inventario):
         super().__init__(parent)
         self.title("Nuevo Producto")
-        self.geometry("350x300")
+        self.geometry("350x280")
         self.inventario = inventario
 
         # Esto hace que Florencia tenga que cerrar esta ventana antes de volver a tocar la principal
         self.grab_set()
+
+        self.label_entrada = ctk.CTkLabel(self, text="Escaneá el código de barras: ", font=("Arial", 15))
+        self.label_entrada.pack(pady = 5)
+
+        #Lector codigo de barras
+        self.entrada_codigo = ctk.CTkEntry(self, placeholder_text="Ej: 0123456748910", width=300, height=15)
+        self.entrada_codigo.pack(pady = 5)
+
+        self.label_nombre_producto = ctk.CTkLabel(self, text="Coloca el nombre del producto: ", font=("Arial", 15))
+        self.label_nombre_producto.pack(pady = 5)
+
+        #Entrada nombre
+        self.entrada_nombre_producto = ctk.CTkEntry(self, placeholder_text="Yerba 'Playadito'", width=300, height=15)
+        self.entrada_nombre_producto.pack(pady = 5)
+
+        self.label_precio_producto = ctk.CTkLabel(self, text="Coloca el precio del producto: ", font=("Arial", 15))
+        self.label_precio_producto.pack(pady = 5)
+
+        #Entrada precio
+        self.entrada_precio_producto = ctk.CTkEntry(self, placeholder_text="1500", width=300, height=15)
+        self.entrada_precio_producto.pack(pady = 5)
+
+        #-----CONTINUAR ACAAAAA
+        #self.label_error = ctk.CTkLabel(self, text="")
+
+        #Boton agregar
+        self.boton_finalizar = ctk.CTkButton(self, text="Guardar producto", command=self.add_product)
+        self.boton_finalizar.pack(pady = 15)
+
+    def add_product(self):
+            codigo = self.entrada_codigo.get().strip()
+            nombre = self.entrada_nombre_producto.get().strip()
+            precio_texto = self.entrada_precio_producto.get().strip()
+
+            #No dejar vacios
+            if not codigo or not nombre or not precio_texto:
+                print("Todos los campos son obligatorios.")
+                return
+
+            try:
+                 precio = float(precio_texto)
+            except ValueError:
+                print("Error! El precio ingresado no es valido.")
+                return
+            
+            if db.product_exist(self.inventario,codigo):
+                print("Error: El producto ya existe en el sistema.")
+            else:
+                nuevo_producto = Producto(codigo, nombre, precio)
+                self.inventario.append(nuevo_producto)
+                db.save_file(self.inventario)
+
+                print(f"¡{nombre} agregado con éxito!")
+                self.destroy() # Cierra la ventanita automáticamente al terminar
 
 # --- VENTANA PRINCIPAL DE ADMINISTRACIÓN ---
 class AdminAPP(ctk.CTk):
