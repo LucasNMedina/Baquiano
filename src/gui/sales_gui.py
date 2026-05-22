d17677d64ee89a7d62398c2b79abb6358ed7ca6f
Lucas <lucasnahuelmedina@hotmail.com>
2026-05-21 22:27:15 -0300

    Actualice sales_gui reenombrando variables

--- src/gui/sales_gui.py
+++ src/gui/sales_gui.py
@@ -7,79 +7,82 @@ class SalesAPP(ctk.CTkToplevel):
         self.grab_set()
         self.title("Baquiano - Ventas")
         self.geometry("500x550")
-        #self.database = db.load_file()
-        self.total_cuenta = 0.0
+        self.total_purchease = 0.0
         
-        self.label_entrada = ctk.CTkLabel(self, text="Escaneá el código de barras: ", font=("Arial", 15))
-        self.label_entrada.pack(pady = 5)
+        #Label código de barras
+        self.lbl_entry_barcode = ctk.CTkLabel(self, text="Escaneá el código de barras: ", font=("Arial", 15))
+        self.lbl_entry_barcode.pack(pady = 5)
 
-        #Lector codigo de barras
-        self.entrada_codigo = ctk.CTkEntry(self, placeholder_text="Ej: 0123456748910", width=300)
-        self.entrada_codigo.pack(pady = 5)
-        self.entrada_codigo.bind("<Return>", self.vender_producto)
+        #Lector código de barras
+        self.entry_barcode = ctk.CTkEntry(self, placeholder_text="Ej: 0123456748910", width=300)
+        self.entry_barcode.pack(pady = 5)
+        self.entry_barcode.bind("<Return>", self.sell_product)
 
         #Label de total
-        self.label_total = ctk.CTkLabel(self, text=f"Total: ${self.total_cuenta:.2f}", text_color="orange", font=("Arial", 25))
-        self.label_total.pack(pady = 5)
+        self.lbl_total = ctk.CTkLabel(self, text=f"Total: ${self.total_purchease:.2f}", text_color="orange", font=("Arial", 25))
+        self.lbl_total.pack(pady = 5)
 
-        self.label_mostrar_codigo = ctk.CTkLabel(self, text="")
-        self.label_mostrar_codigo.pack(pady = 5)
+        #Label muestra error
+        self.lbl_error = ctk.CTkLabel(self, text="")
+        self.lbl_error.pack(pady = 5)
 
-        self.lista_compras = ctk.CTkTextbox(self, width=450, height=300, font=("Arial", 20))
-        self.lista_compras.pack(pady = 5)
-        self.lista_compras.configure(state="disabled")
+        #TextBox de productos
+        self.txtbox_product_list = ctk.CTkTextbox(self, width=450, height=300, font=("Arial", 20))
+        self.txtbox_product_list.pack(pady = 5)
+        self.txtbox_product_list.configure(state="disabled")
 
-        self.boton_finalizar = ctk.CTkButton(self, text="Finalizar venta", command=self.finalizar_venta)
-        self.boton_finalizar.pack(pady = 15)
+        #Botón de finalizar
+        self.btn_finish = ctk.CTkButton(self, text="Finalizar venta", command=self.end_sale)
+        self.btn_finish.pack(pady = 15)
 
-    def vender_producto(self, event):
-        #self.inventario = db.load_file() #Cargo el inventario por las dudas.
-        codigo = self.entrada_codigo.get().strip()
-        producto = db.get_product_by_code(codigo)
+    def sell_product(self, event):
+        barcode = self.entry_barcode.get().strip()
+        product = db.get_product_by_code(barcode)
 
-        if db.product_exist(codigo):
-            self.total_cuenta += producto.precio
-            self.label_total.configure(text=f"Total: ${self.total_cuenta:.2f}")
-            self.actualizar_textbox(producto.nombre, producto.precio)
-            self.entrada_codigo.delete(0, "end")
-        elif codigo.startswith("+"):
+        if db.product_exist(barcode):
+            self.lbl_error.configure(text="")
+            self.total_purchease += product.precio
+            self.lbl_total.configure(text=f"Total: ${self.total_purchease:.2f}")
+            self.actualizar_textbox(product.nombre, product.precio)
+            self.entry_barcode.delete(0, "end")
+        elif barcode.startswith("+"):
             try:
-                monto_manual = float(codigo[1:])
-                self.total_cuenta += monto_manual
+                manual_price = float(barcode[1:])
+                self.total_purchease += manual_price
                 
-                self.label_total.configure(text=f"Total: ${self.total_cuenta:.2f}")
-                self.actualizar_textbox("Fiambreria", monto_manual)
-                self.entrada_codigo.delete(0, "end")
+                self.lbl_total.configure(text=f"Total: ${self.total_purchease:.2f}")
+                self.actualizar_textbox("Fiambreria", manual_price)
+                self.entry_barcode.delete(0, "end")
             except ValueError:
-                self.label_mostrar_codigo.configure(text="Debés ingresar un monto valido.")
-                self.entrada_codigo.delete(0, "end")
+                self.lbl_error.configure(text="Debés ingresar un monto valido.")
+                self.entry_barcode.delete(0, "end")
         else:
-            self.label_mostrar_codigo.configure(text="El producto no existe.")
-            self.entrada_codigo.delete(0, "end")
+            self.lbl_error.configure(text="El producto no existe.")
+            self.entry_barcode.delete(0, "end")
 
-    def finalizar_venta(self):
-        self.total_cuenta = 0.0
+    def end_sale(self):
+        self.total_purchease = 0.0
 
         # Limpiar el Textbox:
-        self.lista_compras.configure(state="normal") # Habilitar
-        self.lista_compras.delete("1.0", "end")      # Borrar desde la línea 1, carácter 0 hasta el final 
-        self.lista_compras.configure(state="disabled") # Bloquear
+        self.txtbox_product_list.configure(state="normal") # Habilitar
+        self.txtbox_product_list.delete("1.0", "end")      # Borrar desde la línea 1, carácter 0 hasta el final 
+        self.txtbox_product_list.configure(state="disabled") # Bloquear
 
-        self.label_total.configure(text=f"Total: ${self.total_cuenta:.2f}")
-        self.label_mostrar_codigo.configure(text="¡Muchas Gracias por comprar!")
+        self.lbl_total.configure(text=f"Total: ${self.total_purchease:.2f}")
+        self.lbl_error.configure(text="¡Muchas Gracias por comprar!")
     
     def actualizar_textbox(self, nombre, precio):
         # Configuramos "normal" para podes escribir
-        self.lista_compras.configure(state="normal")
+        self.txtbox_product_list.configure(state="normal")
 
         # Insertamos al final con "end"
-        self.lista_compras.insert("end", f"{nombre} - ${precio:.2f}\n")
+        self.txtbox_product_list.insert("end", f"{nombre} - ${precio:.2f}\n")
 
         #Se bloquea nuevamente
-        self.lista_compras.configure(state="disabled")
+        self.txtbox_product_list.configure(state="disabled")
         
         #Hacer que el scrol baje solo
-        self.lista_compras.see("end")
+        self.txtbox_product_list.see("end")
 
 if __name__ == "__main__":
     app = SalesAPP()
