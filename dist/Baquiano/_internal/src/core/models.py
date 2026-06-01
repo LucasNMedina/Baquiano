827ec926c7b9172e2a00bf3cca800551f938dd9f
Lucas Medina <lmedina@caja.local>
2026-06-01 14:59:51 -0300

    Saco del repositorio las carpetas de compilacion y la base de datos

--- dist/Baquiano/_internal/src/core/models.py
+++
@@ -1,8 +0,0 @@
-class Producto:
-    def __init__(self, code, name, price):
-        self.code = code
-        self.name = name
-        self.price = price
-
-    def __str__(self):
-        return f"Codigo: {self.code}\nProducto: {self.name} - Precio: ${self.price:.2f}"
